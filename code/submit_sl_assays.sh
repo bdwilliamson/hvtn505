59502b151130071cb3ae508d267a9801f91331ef
Brian Williamson <brianw26@uw.edu>
2019-04-25 17:21:35 -0700

    submit all 11 groups

--- code/submit_sl_assays.sh
+++ code/submit_sl_assays.sh
@@ -3,4 +3,4 @@
 ml R/3.5.3-foss-2016b-fh1
 ml jbigkit
 
-sbatch -c10 -p largenode --mem 33G --time=7-0 --array=1-8 ./run_sl_assays.sh
+sbatch -c10 -p largenode --mem 33G --time=7-0 --array=1-11 ./run_sl_assays.sh
