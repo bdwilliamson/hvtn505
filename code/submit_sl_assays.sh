a7217304500622db387ce1e7dca04551b5c3fc7b
Brian Williamson <brianw26@uw.edu>
2021-02-24 09:56:18 -0800

    don't name the arg in outer bash script

--- code/submit_sl_assays.sh
+++ code/submit_sl_assays.sh
@@ -3,4 +3,7 @@
 ml fhR/4.0.2-foss-2019b
 ml jbigkit
 
-sbatch -A gilbert_p -c10 --mem 33G --time=7-0 --array=1-14 ./run_sl_assays.sh --weight-type ${1}
+# Args:
+# 01: weighting type ("aipw" or "ipw")
+
+sbatch -A gilbert_p -c10 --mem 33G --time=7-0 --array=1-14 ./run_sl_assays.sh ${1}
